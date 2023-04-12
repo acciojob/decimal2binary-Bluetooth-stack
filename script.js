c8291710555b376b06cd95ff203c41b37286c5a7
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-12 13:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function binary(dec){
-    let bin = '';
+function threeSum(dec) {
+// write your code here
+	let bin = '';
     while(dec>0){
         let rem = dec%2;
         bin = rem + bin;
         dec = Math.floor(dec/2);
     }
     return bin;
+  
 }
 
-// let bnry = binary(4);
-// console.log(bnry);
-
-module.exports = binary;
+module.exports = threeSum;
